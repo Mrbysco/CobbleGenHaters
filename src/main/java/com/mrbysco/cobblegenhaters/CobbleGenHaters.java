2a88ff4318a9555457d19fee94756e8ec20dfaa0
Mrbysco <Mrbysco@users.noreply.github.com>
2019-09-01 01:10:25 +0200

    Initial Update

--- src/main/java/com/mrbysco/cobblegenhaters/CobbleGenHaters.java
+++ src/main/java/com/mrbysco/cobblegenhaters/CobbleGenHaters.java
@@ -2,6 +2,7 @@ package com.mrbysco.cobblegenhaters;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,19 +15,15 @@ public class CobbleGenHaters {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public CobbleGenHaters() {
-
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
-	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-	public static class HaterHandler {
-		@SubscribeEvent(priority = EventPriority.HIGH)
-		public static void onBlocksRegistry(final BlockEvent.FluidPlaceBlockEvent event) {
-			BlockState state = event.getNewState();
-			if(state.getBlock() != Blocks.OBSIDIAN)
-			{
-				LOGGER.info(state);
-				event.setNewState(event.getOriginalState());
-			}
+	@SubscribeEvent(priority = EventPriority.HIGH)
+	public void blockPlaceEvent(final BlockEvent.FluidPlaceBlockEvent event) {
+		BlockState state = event.getNewState();
+		if(state.getBlock() != Blocks.OBSIDIAN)
+		{
+			event.setNewState(event.getOriginalState());
 		}
 	}
 }
