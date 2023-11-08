de4c46fba758219a52822178ef868c9dab62a029
Mrbysco <Mrbysco@users.noreply.github.com>
2023-11-08 17:05:47 +0100

    Initial update to 1.20.2

--- src/main/java/com/mrbysco/cobblegenhaters/CobbleGenHaters.java
+++ src/main/java/com/mrbysco/cobblegenhaters/CobbleGenHaters.java
@@ -5,18 +5,18 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.BlockEvent;
 
 @Mod("cobblegenhaters")
 public class CobbleGenHaters {
 	public static final TagKey<Block> ALLOWED_BLOCKS = BlockTags.create(new ResourceLocation("cobblegenhaters", "allowed_blocks"));
 
 	public CobbleGenHaters() {
-		MinecraftForge.EVENT_BUS.register(this);
+		NeoForge.EVENT_BUS.register(this);
 	}
 
 	@SubscribeEvent(priority = EventPriority.HIGH)
